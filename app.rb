cf4c0492bd151c0ce837f5631a52b7ec4f1716a3
mortrap <steelall@yandex.ru>
2025-11-19 01:57:31 +0300

    session id

--- app.rb
+++ app.rb
@@ -1,18 +1,44 @@
 require "sinatra"
 require_relative "./lib/sweep"
+require_relative "./lib/bend"
 
-get "/" do
-erb :index
+enable :sessions
+configure do
+  set :server, :puma
 end
 
-post "/calculate" do
+error do
+  e = env['sinatra.error']
+  "Application error: #{e.message}"
+end
+
+get "/" do
+  if session["id"].nil?
+    session["id"] = rand(0..1000) + rand(0..1000)
+    puts "Welcome!"
+  else
+    puts "Welcome back #{session["id"]}"
+  end
+  erb :index
+end
+ 
+
+get "/unset_id" do 
+  session["id"] = nil
+  erb :unset_id
+end
+
+post "/:id/calculate" do
+  puts session["id"]
   me = params[:metal]
+  #puts me
   b_l = params[:b_l].to_f
   th = params[:thickness].to_f
   radius = params[:radius].to_f
   angle = params[:angle].to_f
   bend_1 = params[:bend_1].to_f
   bend_2 = params[:bend_2].to_f
+  # puts session
   @result_sweep = Sweep.calc_sweep(me, b_l, th, radius, angle, bend_1, bend_2)
   @result_effort = Sweep.calc_eff(me, b_l, th, radius)
   erb :result
--- lib/sweep.rb
+++ lib/sweep.rb
@@ -5,19 +5,27 @@ module Vmatrix
   def self.v_matrix(radius)
     case radius
     when (0.8..1)
-      6
+      [6, 4.5]
     when (1.3..1.5)
-      8
+      [8, 6]
     when (1.7..1.8)
-      10
+      [10, 7]
     when (2.0..2.2)
-      12
+      [12, 8.5]
     when (2.5..2.7)
-      16
+      [16, 11]
     when (3.0..3.4)
-      20
+      [20, 14]
     when (3.5..3.7)
-      22
+      [22, 16]
+    when (3.8..4.3)
+      [25, 18]
+    when (4.9..5.1)
+      [30, 22]
+    when (5.6..5.8)
+      [35, 25]
+    when (8.2..8.6)
+      [50, 36]
     else
       puts 'There is no such matrix'
     end
@@ -41,17 +49,17 @@ module Sweep
         rm = 20
       else puts 'There is no such metal'
       end
-      effort = (1.42 * rm * b_l * th**2) / v
+      effort = (1.42 * rm * b_l * th**2) / v[0]
       res_eff = effort / 1000
       puts "Усилие на #{b_l} мм гиба #{res_eff.round(2)}"
-      puts "Матрица для радиуса #{radius} =>V #{v}"
-      {"effort" =>res_eff.round(2), "matrix for #{radius} radius" =>v} 
+      puts "Матрица для радиуса #{radius} =>V #{v[0]}"
+      {"effort" =>res_eff.round(2), "matrix for #{radius} radius" =>v[0], "minimal bend length for this matrix" =>v[1]} 
     end
 
     # puts "Enter the metal, bending length, thickness, inner radius, angle between bends, and two bends in sequence, separated by spaces in sequence, separated by spaces."
     # inputs = gets.split(" ")
     work_array = [me, b_l, th, radius, angle, bend_1, bend_2]
-    puts b_l
+    # puts b_l
     # require_relative "coe"
     # include Effort
     profit_inputs = work_array[2..].map(&:to_f)
@@ -60,8 +68,8 @@ module Sweep
     x_hash = { 0.1 => 0.323, 0.2 => 0.34, 0.3 => 0.356, 0.4 => 0.367,
                0.5 => 0.379, 0.6 => 0.389, 0.7 => 0.4, 0.8 => 0.413, 0.9 => 0.418,
                1.0 => 0.421, 1.2 => 0.426, 1.3 => 0.431, 1.4 => 0.436,
-               1.5 => 0.441, 2 => 0.455, 3 => 0.463, 4 => 0.469, 5 => 0.477,
-               6 => 0.48, 7 => 0.485, 8 => 0.49, 9 => 0.495, 10.0 => 0.5 }
+               1.5 => 0.441, 2.0 => 0.455, 3.0 => 0.463, 4.0 => 0.469, 5.0 => 0.477,
+               6.0 => 0.48, 7.0 => 0.485, 8.0 => 0.49, 9.0 => 0.495, 10.0 => 0.5 }
     radius = profit_inputs[1]
     thickness = profit_inputs[0]
     angle = profit_inputs[2]
@@ -69,6 +77,7 @@ module Sweep
     l2 = profit_inputs[4]
     koef = radius / thickness
     k_factor = x_hash[koef.round(1)]
+    puts koef
     radius_n = radius + k_factor * thickness
     compensation = 1.95 * (radius + thickness)
     bend_allowance = Math::PI * radius_n * angle / 180.0

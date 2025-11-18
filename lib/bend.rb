b9de8f29fd5bfdca4897c6af11af07b75292845d
mortrap <steelall@yandex.ru>
2025-11-19 01:02:19 +0300

    new session id

---
+++ lib/bend.rb
@@ -0,0 +1,9 @@
+
+class Bend
+    def initialize(b_l1, b_l2, angle)
+        @b_l1 = b_l1
+        @b_l2 = b_l2
+        @angle = angle
+        puts(@b_l1, @b_l2, @angle)
+        end
+end
---
+++ lib/coordinates.rb
@@ -0,0 +1,11 @@
+x = 5
+y = -7
+
+# Конструктор
+point = make_point(x, y)
+
+# Селекторы
+get_x(point) # 5
+get_y(point) # -7
+
+to_string(point) # (5, -7)
